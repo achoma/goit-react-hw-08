da8d5ec4857af57d9ba3e5af40e91df78eeb327f
achoma <139071702+achoma@users.noreply.github.com>
2025-01-20 22:22:52 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { App } from "./app.jsx";
+import { App } from "./App.jsx";
 import store, { persistor } from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
 import "modern-normalize";
